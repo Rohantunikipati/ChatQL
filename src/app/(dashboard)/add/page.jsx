0aaba7b19643a4e629373a0d21ada3abbbd5f61c
Rohantunikipati <rohantunikipat@gamil.com>
2024-08-17 20:18:25 +0530

    Fixed a auth bug

--- src/app/(dashboard)/add/page.jsx
+++ src/app/(dashboard)/add/page.jsx
@@ -1,11 +1,12 @@
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import { AddFriend } from "@/components/AddFriend";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
-
 
 const Add = async () => {
   const session = await getServerSession(options);
+  if (!session) redirect("/");
   return (
     <div>
       <AddFriend user={session.user} />
--- src/app/(dashboard)/request/page.jsx
+++ src/app/(dashboard)/request/page.jsx
@@ -2,10 +2,12 @@ import { options } from "@/app/api/auth/[...nextauth]/options";
 import RequestHandler from "@/components/ui/RequestHandler";
 import { prisma } from "@/lib/prismaClient";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const Request = async () => {
   const session = await getServerSession(options);
+  if (!session) redirect("/");
   var requests;
 
   try {
@@ -13,9 +15,9 @@ const Request = async () => {
       where: {
         request_receiver_id: session.user.id,
       },
-      include:{
-        request_sender:true
-      }
+      include: {
+        request_sender: true,
+      },
     });
   } catch (error) {
     console.log(error);
